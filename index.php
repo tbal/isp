d070cb4d32dd9e1f1c6c51f5f1849e4e9e8a0964
Tilo Baller <tilo.baller@gmail.com>
2016-01-07 02:13:29 +0100

    Integrate dotenv

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
+
+// Loads configuration settings from .env file in project root.
+$dotenv = new \Dotenv\Dotenv(__DIR__);
+$dotenv->overload();
+$dotenv->required(array('DB_HOST', 'DB_USER', 'DB_NAME'))->notEmpty();
+$dotenv->required('DB_PASS');
+
 new \TiloBaller\Application();
